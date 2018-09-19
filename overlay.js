ef9c37b22b19e8817bbd61b1d2631833ad31ef00
Abhinav Gupta <abhinav.g@students.iiit.ac.in>
2018-09-20 03:10:22 +0530

    commit

---
+++ overlay.js
@@ -0,0 +1,24 @@
+function Overlay() 
+{
+	var x = document.getElementById("myTopnav");
+    	if (x.className === "topnav") 
+        {
+                x.className += " responsive";
+        } 
+        else 
+        {
+                x.className = "topnav";
+        }
+}
+  
+function on()
+{
+	document.getElementById("overlay").style.display = "block";
+}
+
+function off() 
+{
+    	document.getElementById("overlay").style.display = "none";
+}
+
+
